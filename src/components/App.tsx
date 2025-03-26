c0fa8165e2d634ae68e2ed798e6c3dbe739bd85d
Nicolas Baracaldo <ndbaracaldo64@ucatolica.edu.co>
2025-03-26 17:29:05 -0500

    First Commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <>
+      <header>
+        
+      </header>
+      <h1 className="text-3xl font-square">Priced to help you
+      grow and thrive.</h1>
+    </>
+  )
+}
+
+export default App
